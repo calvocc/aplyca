747e6b5249711da836020ef6bee35d11c5e8fb17
calvocc <calvocreativocolombiano@gmail.com>
2020-08-31 11:51:34 -0500

    ajuste

--- src/App.vue
+++ src/App.vue
@@ -45,6 +45,7 @@
 
 <style lang="stylus">
 	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap');
+	
 	body{
 		margin: 0;
 		font-family: 'Roboto', sans-serif;
